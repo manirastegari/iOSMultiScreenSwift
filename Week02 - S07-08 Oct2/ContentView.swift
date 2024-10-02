375263dd324bb6d79bbe3a259ed254f73775cb29
Mani Rastegari <mani.rastegari@gmail.com>
2024-10-02 18:06:52 -0400

    Initial Commit

---
+++ Week02 - S07-08 Oct2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week02 - S07-08 Oct2
+//
+//  Created by Mani Rastegari on 2024-10-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
