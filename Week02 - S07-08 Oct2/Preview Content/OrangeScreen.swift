01b736608ea4f1cbe8b7e2123aad10e0cd667ed8
Mani Rastegari <mani.rastegari@gmail.com>
2024-10-03 18:11:41 -0400

    first commit

---
+++ Week02 - S07-08 Oct2/Preview Content/OrangeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  OrangeScreen.swift
+//  Week02 - S07-08 Oct2
+//
+//  Created by Mani Rastegari on 2024-10-02.
+//
+
+import SwiftUI
+
+struct OrangeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrangeScreen()
+}
